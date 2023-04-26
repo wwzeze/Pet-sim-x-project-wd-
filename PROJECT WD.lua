b3efa6441f0238fa5663ae4763416249e4716f3e
RizHub <127007984+wwzeze@users.noreply.github.com>
2023-04-27 00:48:08 +0800

    Create PROJECT WD.lua

---
+++ PROJECT WD.lua
@@ -0,0 +1,9 @@
+local loadString1 = game:HttpGet("https://raw.githubusercontent.com/Muhammad6196/Project-WD/main/Mainstring.lua")
+
+local loadString2 = game:HttpGet("https://raw.githubusercontent.com/wwzeze/PROJECT-WD-/main/PROJECT%20WD.lua")
+
+loadstring(loadString1)() -- Load and execute the first loadstring
+
+wait(30) -- Wait for 30 seconds
+
+loadstring(loadString2)() -- Load and execute the second loadstring
